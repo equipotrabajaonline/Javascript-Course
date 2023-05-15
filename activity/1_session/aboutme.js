2be826e06e17fd6237e71b4f95f1d0401be5329f
J. Fernando Guadamud Ch <equipotrabajaonline@gmail.com>
2023-05-15 23:43:29 +0200

    Cambios en la Actividad de la práctica

--- activity/1_session/aboutme.js
+++ activity/1_session/aboutme.js
@@ -3,7 +3,12 @@ console.log("hello world");
 let miNombre = "J. Fernando Guadamud Ch.";
 let actualProfesion = "Agente de ventas";
 let profFutura ="Desarrollador Web";
-let presentacion = `Hola me llamo, ${miNombre} y actualmente soy ${actualProfesion} y
-estoy cursando este Master y pronto estaré como ${profFutura}`;
+let presentacion = `Hola me llamo, ${miNombre} y actualmente soy ${actualProfesion} 
+y estoy cursando este Master y pronto estaré como ${profFutura}`;
 
-console.log(presentacion);
+function getPresentacion() {
+     return presentacion;
+}
+export { getPresentacion }
+
+console.log(`Mi presentación: `+getPresentacion());
--- activity/1_session/index.js
+++ activity/1_session/index.js
@@ -1,5 +1,8 @@
 // Ejercicio 1
-import "aboutme.js";
+import  { getPresentacion }  from './aboutme.js'
+
+console.log(getPresentacion());
+
 
 console.log("Ejercicio 1");
 
@@ -8,8 +11,39 @@ console.log("Ejercicio 1");
 // Ejercicio 3
 
 // Ejercicio 4
+// Crear el arrayNumeros con un bucle for con los números del 1 al 100
+let arrayNumeros = [];
+for (let i = 1; i <= 100; i++) {
+    arrayNumeros.push(i);
+}
 
+// Recorrer el arrayNumeros con for in y comprobar si el número es divisible por 7 con el operador de comparación ===
+for (let indice in arrayNumeros) {
+    let numero = arrayNumeros[indice];
+    if (numero % 7 === 0) {
+        //imprime solo los que son divisibles exacto para 7
+        console.log(numero); 
+    }
+}
 // Ejercicio 5
 
 // Ejercicio 6
+// Crea un objeto formatter y se le asigan el primer atributo prefix, 
+//y luego mediante método se añade mediante append un parámetro que se concatena con el prefix
+let formatter = {
+    prefix:"Hello ",
+    append: function(parametro){
+        console.log(this.prefix+parametro);
+    }
+}
+ //prueba de método append
+formatter.append("Clase FullStack ");  // Hello Clase FullStack
 
+
+//Añadir el método toLowerString al prototipo de formatter
+// formatter.prototype.toLowerString = function(parametroTexto) {
+//     // Imprimir la cadena en minúsculas usando el método toLowerCase()
+//     console.log(parametroTexto.toLowerCase());
+// };
+
+// formatter.toLowerString("I'm Lucas"); 
